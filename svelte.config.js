e7a3f7c6d4d7bac07d4551e7847a2278ceb883d9
NicolasArtemio <nicolasartemio@gmail.com>
2025-05-21 19:23:17 -0300

    finish

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 
-export default {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
-  preprocess: vitePreprocess(),
-}
+export default defineConfig({
+  base: 'https://github.com/NicolasArtemio/labarber',
+  plugins: [svelte()],
+});
